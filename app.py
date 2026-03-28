3da5349f84ec6f8667b6fe4197ebf8db341d9c24
liangxinyudashuaige <liang20060709@qq.com>
2026-03-28 12:09:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def index():
     return render_template('landing.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8051)
+    import os
+port = int(os.environ.get('PORT', 8080))
+app.run(host='0.0.0.0', port=port, debug=False)
